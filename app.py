4d0993e38f0013a29ec343a10d3fa484c166f02a
janita999 <janitairfan23@gmail.com>
2025-07-09 14:22:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,7 @@ def download_pdf():
 def root():
     return app.send_static_file('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+   if __name__ == '__main__':
+       port = int(os.environ.get('PORT', 10000))
+       app.run(host='0.0.0.0', port=port)
